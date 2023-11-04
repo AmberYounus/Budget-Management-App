e6143874b16fb8ac835a60a4d2f7fce942e81d32
Amber Younus <amberyounus22@gmail.com>
2023-11-03 23:07:05 -0700

    commit

--- src/Components/AddExpenseForm.js
+++ src/Components/AddExpenseForm.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddExpenseForm =()=>{
+    const [name,setName]= useState('');
+    const [cost,setCost]= useState('');
+
+    const onSubmit =(event)=>{
+        event.preventDefault();
+        alert("name "+name +" cost"+cost)
+    }
     return(
         <form>
             <div className="row">
                 <div className="col-sm">
                     <label htmlFor="name">Name</label>
-                    <input type="text" required='required' className="form-control" id="name" />
+                    <input type="text" required='required' className="form-control" id="name" value={name} onChange={(event)=>{setName(event.target.value)}} />
                 </div>
                 <div className="col-sm">
                     <label htmlFor="cost">Cost</label>
-                    <input type="text" required='required' className="form-control" id="cost" />
+                    <input type="text" required='required' className="form-control" id="cost" value={cost} onChange={(event)=>{setCost(event.target.value)}} />
                 </div>
                 <div className="col-sm">
                    <button type="submit" className="btn btn-primary">Save</button>
