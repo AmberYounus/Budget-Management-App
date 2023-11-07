6bbd4af16005e8267c274fcfabd50ab146612b71
Amber Younus <amberyounus22@gmail.com>
2023-11-07 10:41:37 -0800

    commit

--- src/Components/ExpenseTotal.js
+++ src/Components/ExpenseTotal.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React ,{useContext} from "react";
+import { AppContext } from "../Context/AppContext";
 
 const ExpenseTotal = () => {
+    const {expenses} = useContext(AppContext);
 
+    const totalExpenses= expenses.reduce((total,item)=>{
+        return (total += item.cost)
+    } , 0)
 
+    return(
+        <div className="alert alert-primary">
+            <span>Spent so far: €{totalExpenses}</span>
+        </div>
+    )
 }
-export default ExpenseTotal
+export default ExpenseTotal;
