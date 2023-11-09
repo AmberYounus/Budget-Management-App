02ecd27b1d1d6f944af8f127dbc70ab1cbaba6fb
Amber Younus <amberyounus22@gmail.com>
2023-11-08 23:16:05 -0800

    commit

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React , {useContext} from "react";
 // import { TiDelete } from "react-icons/ti";
+import { AppContext } from "../Context/AppContext";
 
 const ExpenseItem = (props) => {
+  const {dispatch} = useContext(AppContext)
+
+  const handleDeleteExpense = () => {
+    dispatch({
+      type:'DELETE_EXPENSE',
+      payload:props.id,
+    });
+  }
+
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
       {props.name}
       <div>
-        <span className="badge badge-primary badge-fill mr-3">
-        €{props.cost}
-          {/* <TiDelete size="1.5em"></TiDelete> */}
+        <span  className="badge badge-primary badge-fill mr-3">
+        €{props.cost} 
+          {/* <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete> */}
         </span>
       </div>
     </li>
